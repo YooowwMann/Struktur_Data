76aafa137682cb62572df66ef314ab1c750d93dc
YooowwMann <ardijakarta4@gmail.com>
2025-04-30 15:33:44 +0700

    Update pert3 - pert5

---
+++ pert3/cpp/basicdatatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    float b = 3.14;
+    double c = 9.40;
+    char d = 'D';
+    bool e = true;
+    string f ="Hello Wordl";
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+
+}
